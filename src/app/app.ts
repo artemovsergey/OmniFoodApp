5952c22f539974ccef1aaf10705a8704dfc75659
sergey <artik3314@gmail.com>
2025-10-21 19:28:19 +0300

    Список компаний

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { Header } from "./header/header";
 import { Hero } from "./hero/hero";
 import { Step } from "./step/step";
+import { Listlogos } from "./listlogos/listlogos";
 
 @Component({
   selector: 'app-root',
-  imports: [Header, Hero, Step],
+  imports: [Header, Hero, Step, Listlogos],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
---
+++ src/app/listlogos/listlogos.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listlogos',
+  imports: [],
+  templateUrl: './listlogos.html',
+  styleUrl: './listlogos.scss'
+})
+export class Listlogos {
+
+}
